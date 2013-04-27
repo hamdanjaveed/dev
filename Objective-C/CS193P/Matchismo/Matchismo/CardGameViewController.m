5f850de8a88d4d99be8da493bea43570aa1a16c7
Hamdan <hamdan@hamdanjaveed.com>
2013-04-27 16:55:35 -0400

    Matchismo: added a flip count label

--- Objective-C/CS193P/Matchismo/Matchismo/CardGameViewController.m
+++ Objective-C/CS193P/Matchismo/Matchismo/CardGameViewController.m
@@ -9,13 +9,20 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipCountLabel;
+@property (nonatomic) int flipCount;
 @end
 
 @implementation CardGameViewController
 
+- (void)setFlipCount:(int)flipCount {
+    _flipCount = flipCount;
+    self.flipCountLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
+
 - (IBAction)flipCard:(UIButton *)sender {
     sender.selected = !sender.isSelected;
+    self.flipCount++;
 }
 
 @end
